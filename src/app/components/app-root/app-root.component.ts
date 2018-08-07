f4478b9c4d25b4e40b120f2d8d519858c77f4ba4
Vidnik Andrei <sawq1234@tutanota.com>
2018-08-07 18:50:35 +0300

    no changes

--- src/app/components/app-root/app-root.component.ts
+++ src/app/components/app-root/app-root.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app-root.component.html',
-  styleUrls: ['./app-root.component.sass']
+    selector: 'app-root',
+    templateUrl: './app-root.component.html',
+    styleUrls: ['./app-root.component.sass']
 })
 export class AppRootComponent {
 
+    constructor() {
+        console.log('mu mu');
+    }
 }
